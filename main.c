a0dc5ee63444d98e8d660d4256cbe7a07b873315
Rayana Almeida <rayana.almeida@icomp.ufam.edu.br>
2026-05-29 20:54:32 -0400

    feat: add main program to process network data from a file

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv[]) {
+    //valida argumentos
+    if (argc != 2){
+        printf("Uso: %s <arquivo.rawcap>\n", argv[1]);
+        return 1;
+    }
+
+    //abre arquivo
+    FILE *arquivo = fopen(argv[1], "rb");
+    if (arquivo == NULL){
+        perror("Erro ao abrir o arquivo");
+        return 1;
+    }
+
+    //chama funcao pra processar as camadas do protocolo
+    processar_dados_rede(arquivo);
+
+    fclose(arquivo);
+
+    return 0;
+}
